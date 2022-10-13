898a2b33d59088944017a15c66086a680c47a249
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-10-13 12:01:03 +0300

    refactoring with useReducer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useReducer} from 'react';
 
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
@@ -8,41 +8,35 @@ import image from '../img/feedBack.png';
 import image2 from '../img/feedBack2.png';
 
 
-export function App ()  {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+function countFeedBacks (state, action) {
+  switch (action.type) {
+    case "setgood":
+     return {...state, good: state.good + action.playLoad }
+     case "setneutral":
+     return {...state, neutral: state.neutral + action.playLoad }
+     case "setbad":
+      return {...state, bad: state.bad + action.playLoad }
+    default:
+      return state;
+  }
+}
 
+export function App ()  {
+const [state, dispatch] = useReducer(countFeedBacks, {good: 0, neutral: 0, bad: 0})
   const countTotalFeedback = () => {
-    return good + neutral + bad
+    return state.good + state.neutral + state.bad
   };
 
   const countPositiveFeedbackPercentage = () => {
     const persantage = Math.round(
-      (100 / countTotalFeedback()) * good
+      (100 / countTotalFeedback()) * state.good
     );
     
     return persantage;
   };
 
-  const feedbackToAdd = event => {
-    switch (event) {
-        case "good":
-            setGood(prevState => prevState + 1)
-            break;
-        case "neutral":
-          setNeutral(prevState => prevState + 1)
-            break;
-            case "bad":
-              setBad(prevState => prevState + 1)
-                break;
-        default:
-            break;
-    }
+    const stateKeys = Object.keys(state);
 
-  }
-    const stateKeys = Object.keys({ good, neutral, bad });
-    console.log(stateKeys)
   return (
     <>
       <AppImgLeft src={image} />
@@ -50,15 +44,15 @@ export function App ()  {
         <AppHeader>Please leave feedback</AppHeader>
         <FeedbackOptions
           options={stateKeys}
-          onLeaveFeedback={feedbackToAdd}
+          onLeaveFeedback={dispatch}
         />
         {!countTotalFeedback() ? (
           <Notification message="No feedback given" />
         ) : (
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
@@ -71,7 +65,73 @@ export function App ()  {
 export default App;
 
 
-// старая версия
+
+//useState
+//
+// export function App ()  {
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+
+//   const countTotalFeedback = () => {
+//     return good + neutral + bad
+//   };
+
+//   const countPositiveFeedbackPercentage = () => {
+//     const persantage = Math.round(
+//       (100 / countTotalFeedback()) * good
+//     );
+    
+//     return persantage;
+//   };
+
+//   const feedbackToAdd = event => {
+//     switch (event) {
+//         case "good":
+//             setGood(prevState => prevState + 1)
+//             break;
+//         case "neutral":
+//           setNeutral(prevState => prevState + 1)
+//             break;
+//             case "bad":
+//               setBad(prevState => prevState + 1)
+//                 break;
+//         default:
+//             break;
+//     }
+
+//   }
+//     const stateKeys = Object.keys({ good, neutral, bad });
+//     console.log(stateKeys)
+//   return (
+//     <>
+//       <AppImgLeft src={image} />
+//       <AppSection>
+//         <AppHeader>Please leave feedback</AppHeader>
+//         <FeedbackOptions
+//           options={stateKeys}
+//           onLeaveFeedback={feedbackToAdd}
+//         />
+//         {!countTotalFeedback() ? (
+//           <Notification message="No feedback given" />
+//         ) : (
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={countTotalFeedback()}
+//             positivePercentage={countPositiveFeedbackPercentage()}
+//           />
+//         )}
+//       </AppSection>
+//       <AppImgRigth src={image2} />
+//     </>
+//   );
+// }
+// export default App;
+
+
+// STATE
 
 // class App1 extends React.Component {
 //   state = {
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 import { OptionsList, OptionsItem, OptionsBtn } from './FeedbackOptions.styled';
+
+
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <OptionsList>
     {options.map(key => (
       <OptionsItem key={key}>
-        <OptionsBtn onClick={() => onLeaveFeedback(key)}>{key}</OptionsBtn>
+        <OptionsBtn onClick={() => onLeaveFeedback({type: `set${key}`, playLoad: 1})}>{key}</OptionsBtn>
       </OptionsItem>
     ))}
   </OptionsList>
@@ -14,3 +16,21 @@ FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
+
+
+//старая версия
+
+// export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+//   <OptionsList>
+//     {options.map(key => (
+//       <OptionsItem key={key}>
+//         <OptionsBtn onClick={() => onLeaveFeedback(key)}>{key}</OptionsBtn>
+//       </OptionsItem>
+//     ))}
+//   </OptionsList>
+// );
+
+// FeedbackOptions.propTypes = {
+//   options: PropTypes.arrayOf(PropTypes.string).isRequired,
+//   onLeaveFeedback: PropTypes.func.isRequired,
+// };
